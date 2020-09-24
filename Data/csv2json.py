6134c5c00531cc52ec71e8dd887ee9eb48a62565
cw <ckddn2820@gmail.com>
2020-09-24 23:15:47 +0900

    csv to json

--- Data/csv2json.py
+++ Data/csv2json.py
@@ -3,13 +3,14 @@ import json
 
 # category.csv
 # csv_path = open('./category.csv', 'r', encoding='utf8')
-# json_path = open('./category.json', 'w')
+# json_path = open('./category.json', 'w', encoding='utf8')
 # fieldnames = ("cid", "pcid", "category", "depth")
 
-# goods.csv
-csv_path = open('./goods.csv', 'r', encoding='utf8')
-json_path = open('./goods.json', 'w', encoding='utf8')
-fieldnames = ("cid", "gname")
+# # dev_data.csv
+csv_path = open('./dev_data.csv', 'r', encoding='utf8')
+json_path = open('./dev_data.json', 'w', encoding='utf8')
+fieldnames = ("goods_id", "category_id", "name", "maker", "model", "image_id"
+              , "cat1", "cat2", "cat3", "cat4", "goods_desc");
 
 reader = csv.DictReader(csv_path, fieldnames)
 
@@ -17,3 +18,6 @@ reader = csv.DictReader(csv_path, fieldnames)
 for row in reader:
     json.dump(row, json_path, ensure_ascii=False)
     json_path.write('\n')
+
+csv_path.close()
+json_path.close()
